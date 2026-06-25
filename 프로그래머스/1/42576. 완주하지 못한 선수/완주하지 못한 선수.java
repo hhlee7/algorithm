3c40b11a466c6554ef98eae44ba0b4a08ec306c3
hhlee7 <gdjlhh777@gmail.com>
2026-06-26 01:46:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 51.32 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,21 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {        
+    public String solution(String[] participant, String[] completion) {
+        
         HashMap<String, Integer> map = new HashMap<>();
         
-        for(String name : participant) {
+        for (String name : participant) {
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
-        
-        for(String name : completion) {
+
+        for (String name : completion) {
             map.put(name, map.getOrDefault(name, 0) - 1);
         }
-        
-        for(String name : map.keySet()) {
-            if(map.get(name) != 0) {
-                return name;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue().intValue() != 0) {
+                return entry.getKey();
             }
         }
         return "";
